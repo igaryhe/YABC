3cc0804e8fd8b39c8dd85f5ce9ce24065f6aeb39
BuptBoyang <zby.che9@gmail.com>
2019-06-02 02:03:07 +0800

    fragment_subject

--- app/src/main/java/io/igaryhe/yabc/viewModels/SubjectMediumViewModel.kt
+++ app/src/main/java/io/igaryhe/yabc/viewModels/SubjectMediumViewModel.kt
@@ -1,8 +1,10 @@
 package io.igaryhe.yabc.viewModels
 
+import android.provider.Telephony
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import io.igaryhe.yabc.models.Rating
 import io.igaryhe.yabc.repositories.SubjectRepository
 
 class SubjectMediumViewModel(private val _id: Int): ViewModel(){
@@ -12,4 +14,5 @@ class SubjectMediumViewModel(private val _id: Int): ViewModel(){
     val name: LiveData<String> = Transformations.map(repo.subjectMedium) {subject -> subject.name}
     val nameCn: LiveData<String> = Transformations.map(repo.subjectMedium) {subject -> subject.nameCn}
     val summary: LiveData<String> = Transformations.map(repo.subjectMedium) {subject -> subject.summary}
+    val score: LiveData<Float> = Transformations.map(repo.subjectMedium) {subject -> subject.rating.score}
 }
