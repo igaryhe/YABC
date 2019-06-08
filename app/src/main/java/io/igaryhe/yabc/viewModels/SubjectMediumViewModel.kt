c7cfc2ad3f4bb4d9306ba7ee4714766c24acc1a4
BuptBoyang <zby.che9@gmail.com>
2019-06-08 18:52:31 +0800

    解决了一个闪退，两个闪退冒了出来

--- app/src/main/java/io/igaryhe/yabc/viewModels/SubjectMediumViewModel.kt
+++ app/src/main/java/io/igaryhe/yabc/viewModels/SubjectMediumViewModel.kt
@@ -17,7 +17,7 @@ class SubjectMediumViewModel(private val _id: Int): ViewModel(){
     val name: LiveData<String> = Transformations.map(repo.subjectMedium) {subject -> subject.name}
     val nameCn: LiveData<String> = Transformations.map(repo.subjectMedium) {subject -> subject.nameCn}
     val summary: LiveData<String> = Transformations.map(repo.subjectMedium) {subject -> subject.summary}
-    val score: LiveData<Float> = Transformations.map(repo.subjectMedium) {subject -> subject.rating.score}
+    val score: LiveData<Float> = Transformations.map(repo.subjectMedium) {subject -> subject?.rating?.score ?:0.0f}
     val rating: LiveData<Rating> = Transformations.map(repo.subjectMedium) {subject -> subject.rating}
     val rank: LiveData<Int> = Transformations.map(repo.subjectMedium) {subject -> subject.rank}
     val crt: LiveData<List<Crt>> = Transformations.map(repo.subjectMedium) {subject -> subject.crt}
